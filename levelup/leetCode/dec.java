07b892658527ea7ba5e4c8a2713894e0eed178de
UtsavRaj21 <utsav11115@gmail.com>
2021-12-05 12:21:42 +0530

    update

--- levelup/leetCode/dec.java
+++ levelup/leetCode/dec.java
@@ -1,51 +1,60 @@
 import java.util.*;
 
-public class dec{
+public class dec {
 
-    //1:-198. House Robber :- Using dp
+    // 1:-198. House Robber :- Using dp
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        if(nums.length == 1){
+        if (nums.length == 1) {
             return nums[0];
         }
-        if(nums.length == 2){
+        if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        if(nums.length == 3){
-            return Math.max(nums[0]+nums[2], nums[1]);
+        if (nums.length == 3) {
+            return Math.max(nums[0] + nums[2], nums[1]);
         }
         dp[0] = nums[0];
         dp[1] = nums[1];
         dp[2] = nums[0] + nums[2];
 
-        for(int i = 3 ; i < n ; i++){
-            int max = Math.max(dp[i-2],dp[i-3]);
+        for (int i = 3; i < n; i++) {
+            int max = Math.max(dp[i - 2], dp[i - 3]);
             dp[i] = nums[i] + max;
         }
-        return Math.max(dp[n-1],dp[n-2]);
-        
+        return Math.max(dp[n - 1], dp[n - 2]);
+
     }
-    
-    //2:328. Odd Even Linked List
+
+    // 2:328. Odd Even Linked List
     public class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
-   
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     public ListNode oddEvenList(ListNode head) {
-        ListNode even = new ListNode() ;
-        ListNode odd = new ListNode() ;
+        ListNode even = new ListNode();
+        ListNode odd = new ListNode();
         ListNode e = even, o = odd;
         ListNode curr = head;
-        while(curr != null){
-            if(curr.val % 2 == 0){
+        while (curr != null) {
+            if (curr.val % 2 == 0) {
                 e.next = new ListNode(curr.val);
                 e = e.next;
-            }else{
+            } else {
                 o.next = new ListNode(curr.val);
                 o = o.next;
             }
@@ -53,41 +62,141 @@ public class dec{
         }
         o.next = even.next;
         e.next = null;
-        return odd.next ;
+        return odd.next;
 
     }
-    
-    //3: 152. Maximum Product Subarray
+
+    // 3: 152. Maximum Product Subarray
     public int maxProduct(int[] nums) {
-        int res = -(int)1e9 ;
+        int res = -(int) 1e9;
         int ans = 1;
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] == 0){
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
                 ans = 1;
-                res = Math.max(0,res);
-            }else{
+                res = Math.max(0, res);
+            } else {
                 ans = ans * nums[i];
-                res = Math.max(ans,res);
+                res = Math.max(ans, res);
             }
-           
+
         }
-         ans =1;
-        
-        for(int i = nums.length-1 ; i >=0; i--){
-            if(nums[i] == 0){
+        ans = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 0) {
                 ans = 1;
-                res = Math.max(0,res);
-            }else{
+                res = Math.max(0, res);
+            } else {
                 ans = ans * nums[i];
-                res = Math.max(ans,res);
+                res = Math.max(ans, res);
             }
-           
+
         }
-        
+
         return res;
     }
+
+    // 4 : 1032. Stream of Characters
+    class StreamChecker {
+
+        public class Node {
+            Node[] children;
+            boolean isEnd;
+
+            Node() {
+                this.children = new Node[26];
+                this.isEnd = false;
+            }
+        }
+
+        Node root;
+
+        StringBuilder sb;
+
+        public void insert(String word, Node root) {
+            Node ptr = root;
+            for (int i = word.length() - 1; i >= 0; i--) {
+                char ch = word.charAt(i);
+                if (ptr.children[ch - 'a'] == null) {
+                    ptr.children[ch - 'a'] = new Node();
+                }
+                ptr = ptr.children[ch - 'a'];
+            }
+            ptr.isEnd = true;
+        }
+
+        public StreamChecker(String[] words) {
+            root = new Node();
+            sb = new StringBuilder();
+            for (String word : words) {
+                insert(word, root);
+            }
+        }
+
+        public boolean find() {
+            Node ptr = root;
+            for (int i = sb.length() - 1; i >= 0; i--) {
+                char ch = sb.charAt(i);
+                if (ptr.children[ch - 'a'] == null) {
+                    return false;
+                }
+                ptr = ptr.children[ch - 'a'];
+                if (ptr.isEnd == true) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public boolean query(char letter) {
+
+            sb.append(letter);
+            return find();
+        }
+    }
+
+    // 5 : 337. House Robber III
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int[] robHouse(TreeNode root){
+        if(root ==  null){
+            return new int[2];
+        }
+
+        int[] left = robHouse(root.left) ;
+        int[] right = robHouse(root.right) ;
+
+        int[] ans = new int[2];
+        ans[0] = left[1] + right[1] + root.val;
+        ans[1] = Math.max(left[0],left[1]) + Math.max(right[0], right[1]);
+
+        return ans;
+    }
+   
+    public int rob(TreeNode root) {
+        int[] ans = robHouse(root);
+        return Math.max(ans[0], ans[1]);
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
