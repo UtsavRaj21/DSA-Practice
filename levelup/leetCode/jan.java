d33db489f1eac4cfbaf87c4cd516ce36fa7bbfc1
UtsavRaj21 <utsav11115@gmail.com>
2022-01-03 14:18:29 +0530

    update

--- levelup/leetCode/jan.java
+++ levelup/leetCode/jan.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class jan {
 
     //
@@ -24,8 +25,37 @@ public class jan {
             }
         }
         return count;
-    }
+
+        //997. Find the Town Judge
+         public int findJudge(int n, int[][] trust) {
+            if(n ==  1 && trust.length == 0) return 1;
+            HashMap<Integer,Integer> map = new HashMap<>();
+             HashSet<Integer> set = new HashSet<>();
+                 for(int[] pair : trust){
+                     int a = pair[0];
+                     int b = pair[1];
+                     set.add(a);
+                     if(!map.containsKey(b)){
+                          map.put(b,1);
+                     }else{
+                          map.put(b,map.get(b) + 1);
+                     }
+                 }
+     
+                 for(int key : map.keySet()){
+                     if(map.get(key) == n-1){
+                         if(!set.contains(key)){
+                             return key;
+                         }
+                         
+                     }
+                 }
+     
+                 return -1;
+        }
+    
+
     public static void main(String[] args) {
-        
+
     }
 }
