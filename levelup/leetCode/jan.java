e237ce9eeeef766a4e62205a5441b40ef646a10c
UtsavRaj21 <utsav11115@gmail.com>
2022-01-13 11:38:28 +0530

    update

--- levelup/leetCode/jan.java
+++ levelup/leetCode/jan.java
@@ -201,6 +201,22 @@ public class jan {
     return sb.toString();
     }
 
+    //452. Minimum Number of Arrows to Burst Balloons
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points,(a,b)->
+          Integer.compare(a[1],b[1])
+          );
+        int arrows = 1 , end = points[0][1];
+        for(int i = 1 ; i < points.length ; i++){
+            if(points[i][0] > end){
+                arrows++;
+                end = points[i][1];
+            }
+        }
+
+        return arrows;
+    }
+
     public static void main(String[] args) {
 
     }
