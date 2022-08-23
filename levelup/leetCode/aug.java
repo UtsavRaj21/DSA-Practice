258ce27ddc448fa25450fdf22e1f553e1c0b71ac
UtsavRaj21 <utsav11115@gmail.com>
2022-08-23 12:00:31 +0530

    update

--- levelup/leetCode/aug.java
+++ levelup/leetCode/aug.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 public class aug{
 
     //659. Split Array into Consecutive Subsequences
@@ -36,7 +39,6 @@ public class aug{
     }
 
     //871. Minimum Number of Refueling Stops
-    class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         if(target <= startFuel){
             return 0;
@@ -60,8 +62,71 @@ public class aug{
         }
         
         return level;
+        }
+    
+
+    
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public ListNode mid(ListNode head){
+        if(head== null || head.next == null){
+            return head;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
     }
-}
+    
+    public ListNode reverse(ListNode head){
+        if(head== null || head.next == null){
+            return head;
+        }
+        
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+           ListNode forw = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        
+        return prev;
+    }
+    
+    public boolean isPalindrome(ListNode head) {
+        if(head== null || head.next == null){
+            return true;
+        }
+        
+        ListNode mid = mid(head);
+        ListNode nhead = mid.next;
+        mid.next = null;
+        nhead = reverse(nhead);
+        ListNode curr1 = head, curr2 = nhead;
+        while(curr2!=null){
+            if(curr1.val != curr2.val){
+                return false;
+            }
+            curr1 = curr1.next;
+            curr2 = curr2.next;
+        }
+        
+        return true;
+    }
     public static void main(String[] args) {
         
     }
